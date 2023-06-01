3e9c2489507f0e5d1f2d166a6c923e297b30f2ef
Binyam T. Fisseha <benomi@protonmail.com>
2023-06-02 01:24:40 +0300

    remove fields

--- resources/customer.go
+++ resources/customer.go
@@ -1,16 +1,16 @@
 package resources
 
 type Customer struct {
-	Addresses            []Address     `json:"addresses"`
-	BusinessVatID        string        `json:"business_vat_id"`
-	Coupon               string        `json:"coupon"`
-	DefaultPaymentMethod string        `json:"default_payment_method"`
-	Description          string        `json:"description"`
-	EWallet              string        `json:"e_wallet"`
+	// Addresses            []Address     `json:"addresses"`
+	// BusinessVatID        string        `json:"business_vat_id"`
+	// Coupon               string        `json:"coupon"`
+	// DefaultPaymentMethod string        `json:"default_payment_method"`
+	// Description          string        `json:"description"`
+	// EWallet              string        `json:"e_wallet"`
 	Name                 string        `json:"name"`
 	Email                string        `json:"email"`
-	Phone                string        `json:"phone_number"`
-	PaymentMethod        PaymentMethod `json:"payment_method"`
+	Phone                *string        `json:"phone_number,omitempty"`
+	// PaymentMethod        PaymentMethod `json:"payment_method"`
 }
 
 type CustomerPaymentMethod struct {
